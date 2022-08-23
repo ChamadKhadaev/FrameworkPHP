0b5bb8c6114e54da83360a673d56fd016c32d038
ChamadKhadaev <chamad.khadaev@epitech.eu>
2022-08-23 13:22:41 +0200

    user login form, email and password 'step 1'

--- src/view/User/login.php
+++ src/view/User/login.php
@@ -1,1 +1,23 @@
 <?php
+
+	require '../../../Core/autoload.php';
+?>
+
+<!Doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>UserLogin</title>
+	<link rel="stylesheet" href="../../../webroot/css/main.css">
+</head>
+	<body>
+		<form method="POST" action="/index.php" enctype="multipart/form-data">
+			<label for="email">Email</label>
+			<input type="email" id="email" name="email"/>
+			<label for="password">Password</label>
+			<input type="password" name="password" id="password"/>
+			<button type="submit">Login</button>
+		</form>
+	</body>
+</html>
