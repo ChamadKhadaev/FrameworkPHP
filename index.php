484768c96e1d81444d71660171a16549323ad4b2
Chamad <chamad.khadaev@epitech.eu>
2022-09-02 23:21:38 +0200

    get Ãmatched routes from Router

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php
+	
+	use Classes\Router\Router;
+	
+	require_once 'Core/autoload.php';
+	
+	$router = new Router($_GET['url']);
+	$router->get('/app', function(){ echo "Bienvenue sur ma homepage !"; });
+	$router->get('/app/:id', function($id){ echo "Voila l'article" . $id; });
+	$router->run();
 
 ?>
+
 <pre>
 	<?= print_r($_POST); print_r($_GET); print_r($_SERVER); ?>
 </pre>
 
-
-<?php
-define ('BASE_URI', str_replace('\\', '/', substr(__DIR__ , strlen($_SERVER ['DOCUMENT_ROOT']))));
-require_once (implode(DIRECTORY_SEPARATOR , ['Core', 'autoload.php']));
-$app = new Core\Core();
-$app->run();
