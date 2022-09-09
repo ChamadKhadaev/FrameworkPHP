e0de435061f3e9b0b44d5406e85299b094a628ed
Chamad <chamad.khadaev@epitech.eu>
2022-09-09 12:52:28 +0200

    RegisterModel=> class RegisterModel=>with methods for creating/registering an user; ex: insertUser()->insert user in database with the given ,  & -->vars are in POO -> ->username etc. UserModel=> class UserModel=>methods to login, loginUsername()->login user if loginUsername()->check username is valid & loginPassword()->check password is valid.. UserModel will contain all methods related to User's Action..

--- src/model/RegisterModel.php
+++ src/model/RegisterModel.php
@@ -3,33 +3,127 @@
 namespace App\Model;
 
 use Core\Database;
+use Exception;
 use PDOException;
 
 class RegisterModel
 {
-    public $username;
-    public $password;
+	/**
+	 * @var string
+	 */
+	public string $username;
+	/**
+	 * @var string
+	 */
+	private string $email;
+	/**
+	 * @var string
+	 */
+	private string $password;
+	
+	/**
+	 * @param string $username
+	 * @param string $email
+	 * @param string $password
+	 */
+	public function __construct(string $username, string $email, string $password)
+	{
+		$this->username = $username;
+		$this->email = $email;
+		$this->password = $password;
+	}
+	
+	/**
+	 * @throws \Exception
+	 */
+	public function createUser($username, $email, $password): bool
+	{
+			$insert = new RegisterModel($username, $email, $password);
+			$insert->insertUser();
+			echo "success createUser";
+			header("Location: /profile/$username");
+			return true;
+	}
+	
+	/**
+	 * @return bool
+	 */
+	public function insertUser(): bool
+	{
+		$this->username = $_POST['username'];
+		$this->email = $_POST['email'];
+		$this->password = $_POST['password'];
+	
+		try {
+			Database::query('INSERT INTO users VALUES(null, :username, :email, :password)',
+				[
+					':username' => $this->username,
+					':email' => $this->email,
+					':password' => password_hash($this->password, PASSWORD_BCRYPT)
+				]
+			);
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+		return TRUE;
+	}
+	
+	/**
+	 * @throws \Exception
+	 */
+	public static function usernameVerify($username): bool
+	{
+		if (!Database::query('SELECT username FROM users WHERE username=:username', [':username' => $username]))
+		{
+			if (strlen($username) >= 4 && strlen($username) <= 32)
+			{
+				if (preg_match('/[a-zA-Z0-9_]+/', $username))
+				{
+					echo "Username Valid <br>";
+					return TRUE;
+				} else {
+					echo "Username must contain only letters and numbers <br>";
+				}
+			} else {
+					echo "Username length must be between 6 and 32 <br>";
+			}
+		} else {
+			echo "Username is already taken <br>";
+		}
+		return false;
+	}
+	
+	public static function passwordVerify($password): bool
+	{
+		if (strlen($password) >= 8 && strlen($password) <= 60)
+		{
+			echo "Password Valid <br>";
+			return True;
+		} else {
+			echo "password must contain between 8 et 60 characters <br>";
+		}
+		return false;
+	}
+	
 
-    public function __construct($username, $password)
-    {
-        $this->username = $username;
-        $this->password = $password;
-    }
-
-    public static function createUserModel()
-    {
-        try {
-            $username = $_POST['username'];
-            $password = $_POST['password'];
-
-            Database::query('INSERT INTO users (username, password) VALUES (:username, :password)',
-                [
-                    'username' => $username,
-                    'password' => $password
-                ]);
-
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
-    }
+	
+	/**
+	 * @throws \Exception
+	 */
+	public static function emailVerify($email): bool
+	{
+		if (filter_var($email, FILTER_VALIDATE_EMAIL))
+		{
+			if (!Database::query("SELECT email FROM users WHERE email = :email", [':email' => $email]))
+			{
+				echo "Email Valid <br>";
+				return True;
+			} else {
+				echo "Email is already taken <br>";
+			}
+		}
+		return false;
+	}
+	
+	
 }
--- src/model/UserModel.php
+++ src/model/UserModel.php
@@ -1,1 +1,94 @@
 <?php
+
+namespace App\Model;
+
+use Classes\Exception\LoginException;
+use Core\Database;
+
+
+class UserModel
+{
+	private int $id;
+	public string $username;
+	private string $email;
+	private string $password;
+	
+	/*public function __construct($id, $username, $email, $password)
+	{
+		$this->id = $id;
+		$this->username = $username;
+		$this->email = $email;
+		$this->password = $password;
+	}*/
+	
+	/**
+	 * @throws \Exception
+	 */
+	public static function getId($id): bool
+	{
+			try{
+				Database::query("SELECT id FROM users WHERE username = :goodtest ", [':id' => $id]);
+				echo "your $id";
+				header("Location: /id/$id");
+				return true;
+			} catch (\PDOException){
+				echo "GetId: not found";
+			}
+		
+		return false;
+	}
+	
+	/**
+	 * @throws \Classes\Exception\LoginException
+	 * @throws \Exception
+	 */
+	public function login(): bool
+	{
+		$this->username = $_POST['username'];
+		$this->password = $_POST['password'];
+		
+
+			if((new UserModel())->loginUsername($this->username)
+				&& (new UserModel())->loginPassword())
+			{
+				echo "success login";
+				session_start();
+				return true;
+			} else {
+				throw new LoginException("Error: UserModel::login() Failed");
+			}
+	}
+	
+	/**
+	 * @throws \Exception
+	 */
+	public function loginUsername($username): bool
+	{
+		$checkUsernameExists = Database::query("SELECT username FROM users WHERE username = :username",
+			[':username' => $username]);
+		
+		if($checkUsernameExists)
+		{
+			return true;
+		} else {
+			throw new LoginException("Error: UserModel::loginUsername($username) not Found!");
+		}
+	}
+	
+	/**
+	 * @throws \Classes\Exception\LoginException
+	 * @throws \Exception
+	 */
+	public function loginPassword(): bool
+	{
+		$checkPasswordHash = Database::query("SELECT password FROM users WHERE password = :password",
+			[':password' => password_verify($this->password, PASSWORD_BCRYPT )]);
+		
+		if($checkPasswordHash)
+		{
+			return true;
+		} else {
+			throw new LoginException("Error: UserModel::loginPassword(password) not Valid!");
+		}
+	}
+}
