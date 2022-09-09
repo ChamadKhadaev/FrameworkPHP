b852dec9ecc7e356b137c60e755c431e5ce26884
Chamad <chamad.khadaev@epitech.eu>
2022-09-09 12:47:44 +0200

    AppController=>redirects to the files with method render(, ) from Core/Controller and UserController=>with methods for User action as loginUser->login user if  and  is true, create()->create/register user if ,  &  are valid

--- src/controller/AppController.php
+++ src/controller/AppController.php
@@ -1,1 +1,30 @@
 <?php
+	namespace App\Controller;
+	
+	require_once $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR . 'Core/autoload.php';
+	
+	use Core\Controller;
+	use Classes\Router\Router;
+
+	
+class AppController extends Controller
+{
+	public function getid(){
+		header('Location: /new.php');
+	}
+	public function homepage()
+	{
+		header('Location: /front.php');
+	}
+	
+	public function register()
+	{
+		header('Location: src/View/User/register.php');
+	}
+	
+	public static function show($slug, $id, $page)
+	{
+		echo "$slug from id:$id, show from src/controller/AppController.php\n
+		page is $page";
+	}
+}
--- src/controller/UserController.php
+++ src/controller/UserController.php
@@ -1,13 +1,68 @@
 <?php
 
-require '../../vendor/autoload.php';
+namespace App\Controller;
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/Core/autoload.php';
 
 use App\Model\RegisterModel;
-use Core\Database;
+use App\Model\UserModel;
+use Core\Controller;
+use Exception;
+use Classes\Exception\RegisterException;
 
-    require '../view/user/register.php';
-    if (isset($_POST['submit']))
-    {
-        RegisterModel::createUserModel();
-    }
+class UserController extends Controller
+{
+	
+	/**
+	 * @throws Exception
+	 */
+	public function create()
+	{
+		$username = $_POST['username'];
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+
+		if (isset($_POST['register']))
+		{
+			if(RegisterModel::usernameVerify($username) && RegisterModel::emailVerify($email) && RegisterModel::passwordVerify($password))
+			{
+				$register = new RegisterModel($username, $email, $password);
+				$register->createUser($username, $email, $password);
+			} else {
+				throw new RegisterException("Error: RegisterModel::createUser($username, $email)");
+			}
+		}
+	}
+	
+	/**
+	 * @throws \Classes\Exception\LoginException
+	 */
+	public function loginUser()
+	{
+		if (isset($_GET['login']))
+		{
+			(new UserModel())->login();
+		}
+	}
+	
+	public function login()
+	{
+		$this->render('login');
+	}
+	
+	/**
+	 * @throws \Exception
+	 */
+	public function getId(): bool
+	{
+		$id = $_GET['getId'];
+		
+		if(isset($_GET['getId']))
+		{
+			UserModel::getId($id);
+			return true;
+		}
+		return false;
+	}
+}
 
